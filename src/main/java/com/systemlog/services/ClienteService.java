bb718380d1f1fa9bf4edd377202faae433d6c756
RodrigoSouzaRocha <rodrigosouzajava@gmail.com>
2021-04-01 14:53:28 -0300

    EndPoint /clientes

---
+++ src/main/java/com/systemlog/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.systemlog.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.systemlog.domain.Cliente;
+import com.systemlog.repositories.ClienteRepository;
+import com.systemlog.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired // Autowired ele auto instancia a depndencia pelo spring 
+	public ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Long id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID:" +  id  + ", tipo: " + Cliente.class.getName()));
+		
+	}
+
+}
