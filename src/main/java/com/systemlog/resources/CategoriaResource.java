9e0d48315e94fc0563839c2e58289b7128284d80
RodrigoSouzaRocha <rodrigosouzajava@gmail.com>
2021-04-30 18:40:25 -0300

    listando objeto com DTO

--- src/main/java/com/systemlog/resources/CategoriaResource.java
+++ src/main/java/com/systemlog/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.systemlog.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.systemlog.domain.Categoria;
+import com.systemlog.dto.CategoriaDTO;
 import com.systemlog.services.CategoriaService;
 
 @RestController
@@ -53,6 +56,17 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete( @PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+
+	}
+
+	@RequestMapping(value = "", method =  RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll( ) {
+		List<Categoria> list = service.findAll();
+		//Stream transforma uma lista em outra lista sem precisar percorrer um for
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+	
+		
+		return ResponseEntity.ok().body(listDTO);
 
 	}
 
--- src/main/java/com/systemlog/services/CategoriaService.java
+++ src/main/java/com/systemlog/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.systemlog.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,10 @@ public class CategoriaService {
 			throw new DataIntegretyException("Não é possivel excluir uma categoria que possui produtos! ");
 		}
 	
+	}
+
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
 	}
 
 }
