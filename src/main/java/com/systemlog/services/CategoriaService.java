8e3720e3c447432dfc449e6932f29edba0204d85
RodrigoSouzaRocha <rodrigosouzajava@gmail.com>
2021-05-05 10:43:36 -0300

    Inserindo um novo cliente com post

--- src/main/java/com/systemlog/services/CategoriaService.java
+++ src/main/java/com/systemlog/services/CategoriaService.java
@@ -3,6 +3,8 @@ package com.systemlog.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -20,31 +22,32 @@ import com.systemlog.services.exceptions.ObjectNotFoundException;
 public class CategoriaService {
 	
 	@Autowired // Autowired ele auto instancia a depndencia pelo spring 
-	public CategoriaRepository categoriaRepository;
+	public CategoriaRepository repository;
 	
 	public Categoria find(Long id) {
-		Optional<Categoria> obj = categoriaRepository.findById(id);
+		Optional<Categoria> obj = repository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID:" +  id  + ", tipo: " + Categoria.class.getName()));
 		
 	}
-	
+
+	@Transactional
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
-		return categoriaRepository.save(obj);
+		return repository.save(obj);
 	}
 
 	public Categoria update(Categoria obj) {
 		Categoria newObj = find(obj.getId());
 		updateData(newObj, obj);
-		return categoriaRepository.save(newObj);
+		return repository.save(newObj);
 
 	}
 
 	public void delete(Long id) {		
 		find(id);
 		try {
-			categoriaRepository.deleteById(id);
+			repository.deleteById(id);
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegretyException("Não é possivel excluir uma categoria que possui produtos! ");
 		}
@@ -52,12 +55,12 @@ public class CategoriaService {
 	}
 
 	public List<Categoria> findAll() {
-		return categoriaRepository.findAll();
+		return repository.findAll();
 	}
 
 	public Page<Categoria> findPage(Integer page, Integer linePage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linePage, Direction.valueOf(direction), orderBy);
-		return categoriaRepository.findAll(pageRequest);
+		return repository.findAll(pageRequest);
 	}
 	
 	public Categoria fromDTO (CategoriaDTO objDTO) {
--- src/main/java/com/systemlog/services/ClienteService.java
+++ src/main/java/com/systemlog/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.systemlog.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.systemlog.domain.Cidade;
 import com.systemlog.domain.Cliente;
+import com.systemlog.domain.Endereco;
 import com.systemlog.dto.ClienteDTO;
+import com.systemlog.dto.ClienteNewDTO;
 import com.systemlog.repositories.ClienteRepository;
+import com.systemlog.repositories.EnderecoRepository;
 import com.systemlog.services.exceptions.DataIntegretyException;
 import com.systemlog.services.exceptions.ObjectNotFoundException;
 
@@ -20,19 +26,30 @@ import com.systemlog.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 	
 	@Autowired // Autowired ele auto instancia a depndencia pelo spring 
-	public ClienteRepository clienteRepository;
+	public ClienteRepository repository;
+	
+	@Autowired
+	public EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Long id) {
-		Optional<Cliente> obj = clienteRepository.findById(id);
+		Optional<Cliente> obj = repository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID:" +  id  + ", tipo: " + Cliente.class.getName()));
 		
 	}
 	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
-		return clienteRepository.save(newObj);
+		return repository.save(newObj);
 
 	}
 
@@ -40,7 +57,7 @@ public class ClienteService {
 		find(id);
 		
 		try {
-			clienteRepository.deleteById(id);
+			repository.deleteById(id);
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegretyException("Não é possivel excluir um cliente porque há entidades relacionadas! ");
 		}
@@ -48,17 +65,34 @@ public class ClienteService {
 	}
 
 	public List<Cliente> findAll() {
-		return clienteRepository.findAll();
+		return repository.findAll();
 	}
 
 	public Page<Cliente> findPage(Integer page, Integer linePage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linePage, Direction.valueOf(direction), orderBy);
-		return clienteRepository.findAll(pageRequest);
+		return repository.findAll(pageRequest);
 	}
 	
 	public Cliente fromDTO (ClienteDTO objDTO) {
 		
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+		
+	}
+	
+	public Cliente fromDTO (ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfcnpj(), objDTO.getTipoCliente());
+		Cidade cid = new Cidade(Long.valueOf(objDTO.getCidadeId()), null , null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		
+		if(objDTO.getTelefone2() != null)
+			cli.getTelefones().add(objDTO.getTelefone2());
+		
+		if(objDTO.getTelefone3() != null)
+			cli.getTelefones().add(objDTO.getTelefone2());
+		
+		return cli;
 		
 	}
 	
