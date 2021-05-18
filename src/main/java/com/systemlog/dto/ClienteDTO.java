a3a8053b711c53d29e593057c6dc450c18005bd3
RodrigoSouzaRocha <rodrigosouzajava@gmail.com>
2021-05-18 20:20:40 -0300

    Validação customizada: E-mail não pode ser repetido na atualizacao de um cliente

--- src/main/java/com/systemlog/dto/ClienteDTO.java
+++ src/main/java/com/systemlog/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.systemlog.domain.Cliente;
+import com.systemlog.services.validator.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
---
+++ src/main/java/com/systemlog/services/validator/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.systemlog.services.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.systemlog.domain.Cliente;
+import com.systemlog.dto.ClienteDTO;
+import com.systemlog.repositories.ClienteRepository;
+import com.systemlog.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		Cliente cliente = null;
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Long uriId = Long.parseLong(map.get("id"));
+		
+		cliente = repo.findByEmail(objDto.getEmail());
+		
+		if (cliente != null && !cliente.getId().equals(uriId))
+			list.add(new FieldMessage("email", "email já cadastrado em nossa base de dados"));
+
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
