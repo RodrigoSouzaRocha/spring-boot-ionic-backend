f70e4e76ab88fe3f301346e7d567dccbf24570c5
Rodrigo Souza <rodrigosouzajava@gmail.com>
2021-06-18 16:13:06 -0300

    Busca de produtos por nome e categoria

--- src/main/java/com/systemlog/repositories/ProdutoRepository.java
+++ src/main/java/com/systemlog/repositories/ProdutoRepository.java
@@ -1,11 +1,26 @@
 package com.systemlog.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.systemlog.domain.Categoria;
 import com.systemlog.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+//	@Query("select distinct obj from Produto obj inner join obj.categorias cat where obj.nome like %:nome% and cat in :categorias")
+//	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageableRequest);
 
+//	Caso seja acrescentada a anotação query ela subscreve a sintaxe
+//	@Query("select distinct obj from Produto obj inner join obj.categorias cat where obj.nome like %:nome% and cat in :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageableRequest);
+
+	
+	
 }
