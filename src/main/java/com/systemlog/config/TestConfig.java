40d1de86f51748795a09c7b22fe035933d92f9ac
Rodrigo Souza <rodrigosouzajava@gmail.com>
2021-06-22 10:05:16 -0300

    Created: criando profile de teste

---
+++ src/main/java/com/systemlog/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.systemlog.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.systemlog.services.DBServices;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBServices dbServices;
+	
+	@Bean
+	public boolean InstantiateDatabase() throws ParseException {
+		
+		dbServices.InstatiateTestDatabase();
+		
+		return true;
+		
+	}
+
+}
