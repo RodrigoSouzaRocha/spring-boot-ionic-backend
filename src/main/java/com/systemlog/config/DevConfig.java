d91217aca271580e416f57da9a2338e562c43716
Rodrigo Souza <rodrigosouzajava@gmail.com>
2021-06-25 09:46:16 -0300

    Atualizando apontamento do banco de dados para o heroku

--- src/main/java/com/systemlog/config/DevConfig.java
+++ src/main/java/com/systemlog/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.systemlog.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,8 +17,14 @@ public class DevConfig {
 	@Autowired
 	private DBServices dbServices;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean InstantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy))
+			return false;
 		
 		dbServices.InstatiateTestDatabase();
 		
